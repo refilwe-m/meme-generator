71ad3c4cfc3449164bf13628bc125aafc239dc5e
Refilwe-M-Mashile <=>
2023-03-14 23:42:32 +0200

    functionality to get image from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Header } from "./components/header/header";
 import { Form } from "./components/form/form";
-import { placeholderMeme } from "./assets";
 import "./App.css";
 
 function App() {
-  const [meme, setMeme] = useState(placeholderMeme);
-
   return (
     <div className="App">
       <Header />
       <main>
         <Form />
-        <section className="results">
-          <img className="meme" src={meme} />
-          <p className="text1">Top text</p>
-          <p className="text2">Bottom text</p>
-        </section>
       </main>
     </div>
   );
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { placeholderMeme } from "../../assets";
+
+const URL = "https://api.imgflip.com/get_memes";
+const getImages = async () => {
+  const response = await fetch(URL);
+  const res = await response.json();
+  return res.data.memes;
+};
+
+const getRandomImage = async () => {
+  const images = await getImages();
+  const randomIndex = Math.floor(Math.random() * images.length);
+  return images[randomIndex].url;
+};
 
 export const Form = () => {
+  const [meme, setMeme] = useState(placeholderMeme);
+
   return (
-    <form>
+    <div>
       <section className="input-fields">
         <input placeholder="Top text" />
         <input placeholder="Bottom text" />
       </section>
 
-      <button className="button">Get a new meme image</button>
-    </form>
+      <button
+        className="button"
+        onClick={async () => {
+          const newMeme = await getRandomImage();
+          setMeme(newMeme);
+        }}
+      >
+        Get a new meme image
+      </button>
+      <section className="results">
+        <img className="meme" src={meme} />
+        <p className="text1">Top text</p>
+        <p className="text2">Bottom text</p>
+      </section>
+    </div>
   );
 };
